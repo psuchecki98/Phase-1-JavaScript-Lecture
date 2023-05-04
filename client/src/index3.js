6a726ef80c87c2ea9e9881efe253036ee3f20309
Patryk <sucheckipatryk@gmail.com>
2023-05-04 15:40:44 -0400

    Lecture May 4th, 2023

--- client/src/index3.js
+++ client/src/index3.js
@@ -6,3 +6,14 @@
 fetch("http://localhost:3000/books")
     .then((resp) => resp.json())
     .then((data) => console.log(data))
+
+function renderBooks(books) {
+    for (let i = 0; i < books.length; i++) {
+        let title = books[i].title
+
+        let h1 = document.createElement('h1')
+        body.appendChild(h1)
+
+        h1.textContent = title
+    }
+}
